1a68354443a7c65e7b21013e18de8e4919432a91
yeferson321 <131elyefer@gmail.com>
2024-06-24 07:38:19 -0500

    feat: version 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,9 @@
 import { defineConfig } from 'astro/config';
-import node from "@astrojs/node";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'server',
-  adapter: node({
-    mode: "standalone"
-  })
+ /*  output: 'server', */
 });
