b38f12442a078fecc978b5dd4885f13ed41fcbfd
yeferson321 <131elyefer@gmail.com>
2024-07-22 00:57:34 -0500

    feat: version 23

--- src/pages/api/data/index.ts
+++ src/pages/api/data/index.ts
@@ -1,37 +1,8 @@
-// Salidas: /builtwith.json
-
-/* export async function GET({params, request}) {
-    return new Response(
-        JSON.stringify({
-           url: 'https://astro.build/'
-        })
-    )
-}
- */
-
-/* 
-import type { APIRoute } from 'astro';
-
-const usernames = ["Sarah", "Chris", "Dan", "Yan", "Elian"]
-
-export const GET: APIRoute = ({ params, request }) => {
-  const id = params.id;
-  return new Response(
-    JSON.stringify({
-      name: usernames[id]
-    })
-  )
-} */
-
-/* import type { APIRoute } from 'astro';
-
-export const GET: APIRoute = async ({ params, request }) => {...} */
-
 
 
 import type { APIRoute } from 'astro';
 
-export const GET: APIRoute = ({ params, request }) => {
+export const GET: APIRoute = () => {
   return new Response(JSON.stringify({ message: "¡Esto es un GET!"}), {
       status: 200,
       headers: {
